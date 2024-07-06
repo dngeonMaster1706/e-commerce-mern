518c30abe0c5af5b3e3732eee24c1aaa5fef1e4a
dngeonMasteer <kaushikborah4080@gmail.com>
2024-07-07 01:40:53 +0530

    Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>
-      Welcome
-    </div>
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+    </>
   )
 }
 

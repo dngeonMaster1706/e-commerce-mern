2967e6221200c82c8a7f1e2eb6fa66ec93e0ff4a
dngeonMasteer <kaushikborah4080@gmail.com>
2024-07-07 02:48:23 +0530

    implemented routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 const App = () => {
   return (
@@ -7,9 +10,10 @@ const App = () => {
       <Header/>
       <main className='py-3'>
         <Container>
-          <h1>Welcome</h1>
+          <Outlet/>
         </Container>
       </main>
+      <Footer/>
     </>
   )
 }

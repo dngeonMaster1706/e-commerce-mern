b8229a8ab10889f7ecfae873120047f6313e4a4f
dngeonMasteer <kaushikborah4080@gmail.com>
2024-07-08 02:21:19 +0530

    fetching with redux

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,5 +5,5 @@ import {getProducts,getProductById} from '../controllers/productController.js'
 router.route('/').get(getProducts)
 router.route('/:id').get(getProductById)
 
-
+ 
 export default router 

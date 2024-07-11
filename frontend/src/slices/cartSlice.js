6bb0f846422188b9c93d15d77bcf8048ccff9fa8
dngeonMasteer <kaushikborah4080@gmail.com>
2024-07-12 01:42:03 +0530

    added cart screen

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -3,7 +3,11 @@ import {createSlice} from '@reduxjs/toolkit'
 import {updateCart} from '../utils/cartUtils'
 
 
-const initialState=localStorage.getItem("cart")? JSON.parse(localStorage.getItem("cart")):{cartItems:[]}
+const initialState=localStorage.getItem("cart")? JSON.parse(localStorage.getItem("cart")):{
+    cartItems:[],
+    shippingAddress:{},
+    paymentMethod:'PayPal'
+};
 const cartSlice=createSlice({
     name:"cart",
     initialState,
@@ -24,9 +28,13 @@ const cartSlice=createSlice({
             state.cartItems=state.cartItems.filter((x)=>x._id !==action.payload);
 
             return updateCart(state)
+        },
+        saveShippingAddress:(state,action)=>{
+            state.shippingAddress=action.payload;
+            return updateCart(state);
         }
     }
 })
-export const {addToCart,removeFromCart}=cartSlice.actions  
+export const {addToCart,removeFromCart,saveShippingAddress}=cartSlice.actions  
 
 export default cartSlice.reducer
